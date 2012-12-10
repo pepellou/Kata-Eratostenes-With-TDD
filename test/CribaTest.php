89e73f13ef80931571ac0d41299f3a2e39789ed4
Pepe Doval <pepellou@gmail.com>
2012-12-10 20:17:58 +0100

    Initial SetUp
    phpunit.xml to run phpunit without args and colored output.
    
    Sample failing test to start kata from here.

---
+++ test/CribaTest.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once (dirname(__FILE__)."/../src/Criba.php");
+
+class CribaTest extends PHPUnit_Framework_TestCase {
+
+	public function test_sample(
+	) {
+		$this->fail("Ouch!");
+	}
+
+}

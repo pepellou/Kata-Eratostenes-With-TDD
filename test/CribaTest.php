e411fef3b58c9c45fc34073b13774498fcad92a7
Pepe Doval <pepellou@gmail.com>
2012-12-10 21:12:11 +0100

    Add exhaustive GREEN test cases

--- test/CribaTest.php
+++ test/CribaTest.php
@@ -25,8 +25,18 @@ class CribaTest extends PHPUnit_Framework_TestCase {
 			"up to 2" => array(2, array(2)),
 			"up to 3" => array(3, array(2, 3)),
 			"up to 4" => array(4, array(2, 3)),
+			"up to 5" => array(5, array(2, 3, 5)),
 			"up to 6" => array(6, array(2, 3, 5)),
-			"up to 8" => array(8, array(2, 3, 5, 7))
+			"up to 7" => array(7, array(2, 3, 5, 7)),
+			"up to 8" => array(8, array(2, 3, 5, 7)),
+			"up to 9" => array(9, array(2, 3, 5, 7)),
+			"up to 10" => array(10, array(2, 3, 5, 7)),
+			"up to 11" => array(11, array(2, 3, 5, 7, 11)),
+			"up to 105" => array(105, array(
+				2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+				31, 37, 41, 43, 47, 53, 59, 61, 67,
+				71, 73, 79, 83, 89, 97, 101, 103
+			))
 		);
 	}
 

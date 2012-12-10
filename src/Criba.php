509499af2a39a8137c475e5fb7d552a53d3ec6c6
Pepe Doval <pepellou@gmail.com>
2012-12-10 21:26:55 +0100

    Refactor - output is a criba filter
    Based on the fact that our getPrimes() only implements a filter
    on our "marks" array, we can filter this array and return the keys
    of the resulting filtered array. Simpler :-)

--- src/Criba.php
+++ src/Criba.php
@@ -54,13 +54,7 @@ class Criba {
 
 	public function getPrimes(
 	) {
-		$primes = array();
-		for ($number = 2; $number <= $this->upTo; $number++) {
-			if ($this->isPrime($number)) {
-				$primes []= $number;
-			}
-		}
-		return $primes;
+		return array_keys(array_filter($this->marks));
 	}
 
 }

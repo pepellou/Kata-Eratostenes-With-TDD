36897c89d5b1c8a7d20bfa5b81c66c7ae3a06d00
Pepe Doval <pepellou@gmail.com>
2012-12-10 21:19:17 +0100

    Refactor - extract method

--- src/Criba.php
+++ src/Criba.php
@@ -18,12 +18,18 @@ class Criba {
 	) {
 		$this->cleanCriba();
 		for ($n = 2; $n <= $this->upTo; $n++) {
-			if ($this->marks[$n] == self::$PRIME) {
+			if ($this->isPrime($n)) {
 				for ($multipleOfN = 2 * $n; $multipleOfN <= $this->upTo; $multipleOfN += $n) {
 					$this->marks[$multipleOfN] = !self::$PRIME;
 				}
 			}
 		}
+	}
+
+	private function isPrime(
+		$n
+	) {
+		return ($this->marks[$n] == self::$PRIME);
 	}
 
 	private function cleanCriba(
@@ -38,7 +44,7 @@ class Criba {
 	) {
 		$primes = array();
 		for ($number = 2; $number <= $this->upTo; $number++) {
-			if ($this->marks[$number] == self::$PRIME) {
+			if ($this->isPrime($number)) {
 				$primes []= $number;
 			}
 		}
